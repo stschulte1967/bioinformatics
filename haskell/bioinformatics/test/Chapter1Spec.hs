c46b184e4419441b678ba5ebd1611babd8f3de7d
Stephan Schulte <stephan.schulte@gmail.com>
2025-12-02 23:11:13 +0100

    preparation for more tests

--- haskell/bioinformatics/test/Chapter1Spec.hs
+++ haskell/bioinformatics/test/Chapter1Spec.hs
@@ -5,7 +5,7 @@ import Lib
 
 spec :: Spec
 spec = do
-  describe "patternTest" $ do
+  describe "PatternCount" $ do
     it "simplest possible test" $ do testPatternCount "../../data/PatternCount/1A/inputs/input_1.txt" 2
     it "a little bit more" $ do testPatternCount "../../data/PatternCount/1A/inputs/input_2.txt" 3
     it "test 3" $ do testPatternCount "../../data/PatternCount/1A/inputs/input_3.txt" 4
@@ -14,6 +14,12 @@ spec = do
     it "test 6" $ do testPatternCount "../../data/PatternCount/1A/inputs/input_6.txt" 5
     it "test 7" $ do testPatternCount "../../data/PatternCount/1A/inputs/input_7.txt" 9
     it "test 8" $ do testPatternCount "../../data/PatternCount/1A/inputs/input_8.txt" 294
+
+  describe "FrequentWords" $ do
+    it "test 1" $ do testFrequentWords "../../data/PatternCount/1A/inputs/input_1.txt" 2
+
+sameElements xs ys = sort xs == sort ys 
+sameElements :: (Ord a) => [a] -> [a] -> Bool
 
 testPatternCount filename solution = do    
     content <- readFile filename
